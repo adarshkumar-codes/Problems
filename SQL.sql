cf3b096b16d04c0c5fc4ea1f1689d5df82819550
adarshkumar-codes <adarsh.kumar@coditas.com>
2026-05-04 16:10:45 +0530

    Add SQL query for sales person and average calculation

--- SQL.sql
+++ SQL.sql
@@ -1,3 +1,14 @@
+-- 607. Sales Person
+--   https://leetcode.com/problems/sales-person/description/
+select name
+from salesperson
+where sales_id not in (
+    select o.sales_id
+    from orders o
+    join company c on o.com_id = c.com_id
+    where c.name = 'red'
+);
+
 -- Person's average > Average of the country, Problem from w3schools
 select 
   s.CustomerName, 
