6c63d3e9c7305237b1be1d0fd23e12deef040955
adarshkumar-codes <adarsh.kumar@coditas.com>
2026-05-07 18:12:15 +0530

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,3 +1,9 @@
+-- Unique Users Per Client Per Month
+-- https://platform.stratascratch.com/coding/2024-unique-users-per-client-per-month?code_type=3
+select client_id, month(time_id) as month, count(distinct user_id) as users_num from fact_events
+group by client_id, month;
+
+
 -- 1587. Bank Account Summary II
 -- https://leetcode.com/problems/bank-account-summary-ii/description/
 select u.name as NAME, 
